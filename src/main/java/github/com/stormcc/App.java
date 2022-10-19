623f35d4bdb07f97ff0a6eb4bf13e1c5b6b232dc
宋兴茂 <songxingmao@bonc.com.cn>
2022-10-19 16:31:09 +0800

    bugfix

--- src/main/java/github/com/stormcc/App.java
+++ src/main/java/github/com/stormcc/App.java
@@ -4,8 +4,7 @@ package github.com.stormcc;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
