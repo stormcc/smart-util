1834982a482efa99f6a6776d5bbfe03543a79698
宋兴茂 <songxingmao@bonc.com.cn>
2022-10-22 08:49:42 +0800

    bugfix

--- src/main/java/github/com/stormcc/App.java
+++ src/main/java/github/com/stormcc/App.java
@@ -1,11 +1,14 @@
 package github.com.stormcc;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Hello world!
  *
  */
+@Slf4j
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        log.info( "Hello World!" );
     }
 }
