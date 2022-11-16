67fc10dfd25eda0c6a1aa6b3180ca9d0e7de25e1
宋兴茂 <songxingmao@bonc.com.cn>
2022-11-16 09:44:26 +0800

    bugfix

--- src/main/java/github/com/stormcc/util/JacksonUtil.java
+++ src/main/java/github/com/stormcc/util/JacksonUtil.java
@@ -2,23 +2,29 @@ package github.com.stormcc.util;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.TimeZone;
 
+@Slf4j
 public final class JacksonUtil {
     private JacksonUtil(){}
+
+    private static volatile int serialize = 0;
+    private static volatile int deserialize =0;
     private static ObjectMapper serializeObjectMapper = null;
     private static ObjectMapper deserializeObjectMapper = null;
 
     public static ObjectMapper serializeObjectMapper(){
-        if ( serializeObjectMapper == null ) {
+        if ( serialize == 0 ) {
             synchronized (JacksonUtil.class) {
-                if ( serializeObjectMapper == null ) {
+                if ( serialize == 0 ) {
                     ObjectMapper mapper = new ObjectMapper();
                     mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
                     // 允许单引号字段名
@@ -41,6 +47,7 @@ public final class JacksonUtil {
                     module.addDeserializer(java.sql.Timestamp.class, new DateDeserializers.TimestampDeserializer());
                     mapper.registerModule(module);
                     serializeObjectMapper = mapper;
+                    serialize =1 ;
                 }
             }
         }
@@ -49,15 +56,29 @@ public final class JacksonUtil {
     }
 
     public static ObjectMapper deserializeObjectMapper(){
-        if ( deserializeObjectMapper == null ) {
+        if ( deserialize == 0 ) {
             synchronized (JacksonUtil.class) {
-                if ( deserializeObjectMapper == null ){
+                if ( deserialize == 0 ) {
                     ObjectMapper mapper = new ObjectMapper();
                     mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
                     deserializeObjectMapper = mapper;
+                    deserialize = 1;
                 }
             }
         }
         return deserializeObjectMapper;
+    }
+
+    public static String valueAsString(Object o) {
+        try {
+            return serializeObjectMapper().writeValueAsString(o);
+        } catch (JsonProcessingException e) {
+            log.error("return null, JsonProcessingException is:{}", LogExceptionStackUtil.logExceptionStack(e));
+        }
+        return null;
+    }
+
+    public static String valueAsStringThrow(Object o) throws JsonProcessingException {
+        return deserializeObjectMapper().writeValueAsString(o);
     }
 }
