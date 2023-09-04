92fd934a80f90385e3ad201777c80d4daeb31a74
宋兴茂 <songxingmao@bonc.com.cn>
2023-09-04 22:47:58 +0800

    new add

--- src/main/java/github/com/stormcc/util/FileUtil.java
+++ src/main/java/github/com/stormcc/util/FileUtil.java
@@ -3,6 +3,9 @@ package github.com.stormcc.util;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +13,7 @@ import java.util.List;
  * Create By: Jimmy Song
  * Create At: 2022-09-05 18:15
  */
+@Slf4j
 public final class FileUtil {
     private FileUtil(){}
 
@@ -37,5 +41,41 @@ public final class FileUtil {
                 fillDeepFile(fullPathName, list);
             }
         }
+    }
+
+
+    public static List<String> tailLineList(String fileFullPath, int lineNumber) {
+        List<String> list = new ArrayList<>(lineNumber);
+        try (RandomAccessFile rf = new RandomAccessFile(fileFullPath, "r"); ) {
+            long len = rf.length();
+            long start = rf.getFilePointer();
+            long nextEnd = start + len - 1;
+            String line = null;
+            rf.seek(nextEnd);
+            int c = -1;
+            while (nextEnd > start) {
+                c = rf.read();
+                if (c == '\n' || c == '\r') {
+                    line = rf.readLine();
+                    list.add(line);
+                    if (list.size() == lineNumber) {
+                        break;
+                    }
+                }
+                nextEnd--;
+                rf.seek(nextEnd);
+            }
+        } catch (FileNotFoundException e) {
+            log.error("fileFullPath is:{}, lineNumber is:{}, FileNotFoundException is:{}",
+                    fileFullPath, lineNumber, LogExceptionStackUtil.logExceptionStack(e));
+        } catch (IOException e) {
+            log.error("fileFullPath is:{}, lineNumber is:{}, IOException is:{}",
+                    fileFullPath, lineNumber, LogExceptionStackUtil.logExceptionStack(e));
+        }
+        List<String> list1 = new ArrayList<>(lineNumber);
+        for (int i = list.size()-1; i >=0; i--) {
+            list1.add(list.get(i));
+        }
+        return list1;
     }
 }
