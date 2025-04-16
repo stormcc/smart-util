754a7d51cf66d9a4df675e6f43f42a28b6e3f902
宋兴茂 <songxingmao1@bonc.com.cn>
2025-04-16 21:49:22 +0800

    添加 从镜像文件中读取镜像Id工具类

---
+++ src/main/java/github/com/stormcc/util/SerializeUtil.java
@@ -0,0 +1,52 @@
+package github.com.stormcc.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+@Slf4j
+public final class SerializeUtil {
+
+    /**
+     * 序列化
+     *
+     * @param object
+     * @return
+     */
+    public static byte[] serialize(Object object) {
+        ObjectOutputStream oos = null;
+        ByteArrayOutputStream baos = null;
+        try {
+            // 序列化
+            baos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(baos);
+            oos.writeObject(object);
+            return baos.toByteArray();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    /**
+     * 反序列化
+     *
+     * @param bytes
+     * @return
+     */
+    public static Object unserialize(byte[] bytes) {
+        ByteArrayInputStream bais = null;
+        try {
+            // 反序列化
+            bais = new ByteArrayInputStream(bytes);
+            ObjectInputStream ois = new ObjectInputStream(bais);
+            return ois.readObject();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        return null;
+    }
+}
